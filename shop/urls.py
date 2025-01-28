6819b4c608efa41b41a72a594d49e572f91899a1
bello yusuf <belloyusuf1997@gmail.com>
2025-01-28 12:28:29 +0100

    Create customer app

--- shop/urls.py
+++ shop/urls.py
@@ -7,7 +7,15 @@ app_name = 'product_app'
 
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
-    path('products', views.ListProducts.as_view(), name='product-list'),
-    path("add-product/", views.AddNewProduct.as_view(), name="add-product"),
+    path("", views.dashboard, name="dashboard"),
+    # Product urls
+    path("products-list", views.ListProducts.as_view(), name="product-list"),
+    path("create-product/", views.AddNewProduct.as_view(), name="add-product"),
+    path("update-product/", views.UpdateProducts.as_view(), name="update-product"),
+    path("delete-product/", views.DeleteProduct.as_view(), name="delete-product"),
+    # Category urls
+    path("create-category/", views.CreateCategory.as_view(), name="create-category"),
+    path("list-category/", views.ListCategories.as_view(), name="list-category"),
+    path("update-category/", views.UpdateCategories.as_view(), name="update-category"),
+    path("update-category/", views.DeleteCategories.as_view(), name="delete-category"),
 ]
